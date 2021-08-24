7144d583f71f45661173049c5c929755dc9b4690
Ronald Suwandi <ronald@ronaldsuwandi.com>
2021-08-24 22:46:29 +0800

    WIP - completely not working join

--- stream_join.go
+++ stream_join.go
@@ -15,6 +15,11 @@ func (s *StreamJoiner) InnerJoinTable(t *Table, joinerFn NodeJoinerFn) *Stream {
 	next := NewStream(s.producerChan)
 
 	next.processFn = func(kvc KeyValueContext, src Node) (KeyValueContext, bool) {
+		// TODO
+		// if src == s.source, then it's left
+		// else it's right
+		// if left, check if corresponding right available - look up state store?
+		// if right, check if corresponding left is available - look up in memory cache?
 		joinerFn()
 		vc := fn(kvc)
 		return KeyValueContext{Key: kvc.Key, ValueContext: vc}, true
